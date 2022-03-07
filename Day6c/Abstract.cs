f7b556fb6b86d781ba2009056ee01f433fd9dff0
Lakshita-Garg <lakshita.garg@ibm.com>
2022-03-07 11:30:56 +0530

    Add project files.

---
+++ Day6c/Abstract.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public abstract class stationary
+{
+    public abstract void show();
+    protected int rate;
+    public void cost()
+    {
+        Console.WriteLine("The cost is:" + rate);
+    }
+} 
+public class Pen : stationary
+{
+    public override void show()
+    {
+        rate = 50;
+        Console.WriteLine("Pen");
+    }
+}
+public class Pencil : stationary
+{
+    public override void show()
+    {
+        rate = 20;
+        Console.WriteLine("Pencil");
+    }
+}
+internal class Abstract
+{
+    static void Main(string[] args)
+    {
+        stationary s;
+        Console.WriteLine("For pen:");
+        s = new Pen();
+        s.show();
+        s.cost();
+        Console.WriteLine("For pencil:");
+        s = new Pencil();
+        s.show();
+        s.cost();
+        Console.ReadLine();
+    }
+}
+
---
+++ Day6c/interface.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IFirstin
+    {
+        string Name { get; set; }
+        void firstMethod();
+    }
+    interface ISecondin
+    {
+        void secMethod();
+    }
+    interface IThirdin
+    {
+        void thMethod();
+    }
+
+    class MyClass: IFirstin, ISecondin, IThirdin
+    {
+        public string Name { get; set; }
+        public void firstMethod()
+        {
+            Console.WriteLine("First method");
+        }
+        public void secMethod()
+        {
+            Console.WriteLine("Second method");
+        }
+        public void thMethod()
+        {
+            Console.WriteLine("Third method");
+        }
+    }
+    class main
+    {
+        public static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.Name = "Lakshita garg";
+            myClass.firstMethod();
+            myClass.secMethod();
+            myClass.thMethod();
+            Console.WriteLine(myClass.Name);
+            Console.ReadLine();
+        }
+    }
+
+}
