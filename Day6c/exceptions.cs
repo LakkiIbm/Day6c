040da53437a18ac2a17a56dfba3aaf8992e6c9dd
Lakshita-Garg <lakshita.garg@ibm.com>
2022-03-07 17:23:00 +0530

    Task: 4 exception classes

---
+++ Day6c/exceptions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+//--------------------------Null reference exception----------------------------
+//namespace Day6c
+//{
+
+//    class exceptions
+//    {
+//        static void Main(string[] args)
+//        {
+//            string str = null;
+//            try
+//            {
+//                Console.WriteLine(str.Substring(5));
+//            }
+//            catch (NullReferenceException err)
+//            {
+//                Console.WriteLine("Please check the string str.");
+//                Console.WriteLine(err.Message);
+//            }
+//            Console.WriteLine("Continuing with other statments..");
+//        }
+//    }
+//}
+
+
+//---------------------Invalid cast exceptions------------------------------------------------
+//namespace Day6c
+//{
+
+//    class exceptions
+//    {
+//        static void Main(string[] args)
+//        {
+//            bool s = false;
+//            try
+//            {
+//                Console.WriteLine(Convert.ToChar(s));
+//            }
+//            catch (InvalidCastException err)
+//            {
+//                Console.WriteLine("Please check the string str.");
+//                Console.WriteLine(err.Message);
+//            }
+//            Console.WriteLine("Continuing with other statments..");
+//        }
+//    }
+//}
+
